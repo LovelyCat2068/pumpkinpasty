f30d71f5361a9e8f06c0babdf10cce62240dc9a8
LovelyCat2068 <76818025+LovelyCat2068@users.noreply.github.com>
2021-10-19 20:13:27 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,36 @@
-function preload(){}
+azkaban = 0;
+fortress = 0;
+
+function preload(){
+    g0blet0fFIRE = loadImage('https://i.postimg.cc/PxFvYgkv/l1.png');
+}
 
 function setup(){
     canvas = createCanvas(300, 300);
     canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    pumpkin = ml5.poseNet(video, modelloaded);
+    pumpkin.on('pose', pumpkinjuice);
 }
 
-function draw(){}
+function pumpkinjuice(results){
+    console.log(results);
+    azkaban = results[0].pose.nose.x;
+    fortress = results[0].pose.nose.y;
+}
+
+function draw(){
+    image(video, 0, 0, 300, 300);
+    image(g0blet0fFIRE, azkaban - 230, fortress - 100, 100, 100);
+}
 
 function FloreanFortescue(){
     save("cauldronCakes.jpg");
+}
+
+function modelloaded(){
+    console.log("chamber0fsecrets");
 }
